99857d32a6615bb4e127a0ed98725a8fa05925a5
Gilles-Philippe Morin <6393838+GPhMorin@users.noreply.github.com>
2022-04-16 17:07:14 -0400

    Plein de changements!

--- Server/Server/Source.cpp
+++ Server/Server/Source.cpp
@@ -8,9 +8,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Notre ajouts
+// Nos ajouts
 #include <string>
 #include <direct.h>
+#include <vector>
+#include <fstream>
 
 // https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
 #include <iostream>
@@ -22,7 +24,7 @@
 #pragma comment (lib, "Ws2_32.lib")
 // #pragma comment (lib, "Mswsock.lib")
 
-#define DEFAULT_BUFLEN 500000
+#define DEFAULT_BUFLEN 4096
 #define DEFAULT_PORT "33333"
 
 using namespace std;
@@ -37,7 +39,7 @@ std::string exec(const char* cmd) {
         for (int i = 3; i < strlen(cmd); i++) {
             directory += cmd[i];
         }
-        _chdir(directory.c_str());
+        int i = _chdir(directory.c_str());
         return "OKAY CHDIR";
     }
 
@@ -159,45 +161,138 @@ int __cdecl main(void)
                 char* cmd = recvbuf;
                 cmd[iResult] = '\0';
 
-                // https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
+                string filestring(cmd, strlen(cmd));
+                if (filestring.find("FILETRANSMISSION") == 0) {
+                    vector<unsigned char> buffer;
 
-                string output;
-                output = exec(cmd);
+                    string nb_str(cmd, strlen(cmd));
+                    nb_str.erase(0, 17);
+                    int nb = stoi(nb_str);
 
-                // Echo the buffer back to the sender
-                iSendResult = send(ClientSocket, output.c_str(), output.size(), 0);
-                if (iSendResult == SOCKET_ERROR) {
-                    printf("send failed with error: %d\n", WSAGetLastError());
+                    for (int i = 0; i < nb; i++) {
+                        iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+                        if (iResult > 0) {
+                            printf("Bytes received: %d\n", iResult);
+
+                            for (int i = 0; i < iResult; i++) {
+                                buffer.push_back(recvbuf[i]);
+                            }
+                        }
+                    }
+
+                    // https://stackoverflow.com/questions/15170161/c-winsock-sending-file
+
+                    ofstream output(".\\log.txt", ios::binary);
+                    /*if (output.is_open()) {
+                        output.write(recvbuf, recvbuflen);
+                        ZeroMemory(&recvbuf, recvbuflen);
+                    }*/
+
+                    for (auto it : buffer) {
+                        output << it;
+                    }
+                    output.close();
+                }
+                else if (filestring.find("FILERECEIVE") == 0) {
+                    string location = cmd;
+                    location.erase(0, 12);
+                    ifstream input(location, ios::binary);
+                    vector<char> buffer;
+
+                    // get length of file
+                    input.seekg(0, input.end);
+                    size_t length = input.tellg();
+                    input.seekg(0, input.beg);
+
+                    int nb = ceil((float)length / (float)DEFAULT_BUFLEN);
+                    string message = "FILETRANSMISSION " + to_string(nb);
+
+                    iResult = send(ClientSocket, message.c_str(), message.size(), 0);
+                    if (iResult == SOCKET_ERROR) {
+                        printf("send failed with error: %d\n", WSAGetLastError());
+                        closesocket(ClientSocket);
+                        WSACleanup();
+                        return 1;
+                    }
+                    printf("Bytes Sent: %ld\n", iResult);
+
+                    //read file
+                    if (length > 0) {
+                        buffer.resize(length);
+                        input.read(&buffer[0], length);
+                    }
+
+                    int i = 1;
+                    string part = "";
+                    for (auto it : buffer) {
+                        part += it;
+                        if (i % DEFAULT_BUFLEN == 0) {
+                            iResult = send(ClientSocket, part.c_str(), strlen(part.c_str()), 0);
+                            if (iResult == SOCKET_ERROR) {
+                                printf("send failed with error: %d\n", WSAGetLastError());
+                                closesocket(ClientSocket);
+                                WSACleanup();
+                                return 1;
+                            }
+                            printf("Bytes Sent: %ld\n", iResult);
+
+                            i = 0;
+                            part.clear();
+                        }
+                        i++;
+                    }
+                    iResult = send(ClientSocket, part.c_str(), strlen(part.c_str()), 0);
+                    if (iResult == SOCKET_ERROR) {
+                        printf("send failed with error: %d\n", WSAGetLastError());
+                        closesocket(ClientSocket);
+                        WSACleanup();
+                        return 1;
+                    }
+                    printf("Bytes Sent: %ld\n", iResult);
+                }
+                else {
+                    cmd[iResult] = '\0';
+
+                    // https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
+
+                    string output;
+                    output = exec(cmd);
+
+                    // Echo the buffer back to the sender
+                    iSendResult = send(ClientSocket, output.c_str(), output.size(), 0);
+                    if (iSendResult == SOCKET_ERROR) {
+                        printf("send failed with error: %d\n", WSAGetLastError());
+                        closesocket(ClientSocket);
+                        WSACleanup();
+                        return 1;
+                    }
+                    printf("Bytes sent: %d\n", iSendResult);
+                }
+            }
+            else if (iResult == 0) {
+                    printf("Connection closing...\n");
+                }
+            else {
+                    printf("recv failed with error: %d\n", WSAGetLastError());
                     closesocket(ClientSocket);
                     WSACleanup();
                     return 1;
                 }
-                printf("Bytes sent: %d\n", iSendResult);
-            }
-            else if (iResult == 0)
-                printf("Connection closing...\n");
-            else {
-                printf("recv failed with error: %d\n", WSAGetLastError());
+            } while (iResult > 0);
+
+                // shutdown the connection since we're done
+                iResult = shutdown(ClientSocket, SD_SEND);
+                if (iResult == SOCKET_ERROR) {
+                    printf("shutdown failed with error: %d\n", WSAGetLastError());
+                    closesocket(ClientSocket);
+                    WSACleanup();
+                    return 1;
+                }
+
+                // cleanup
                 closesocket(ClientSocket);
                 WSACleanup();
-                return 1;
             }
 
-        } while (iResult > 0);
-
-        // shutdown the connection since we're done
-        iResult = shutdown(ClientSocket, SD_SEND);
-        if (iResult == SOCKET_ERROR) {
-            printf("shutdown failed with error: %d\n", WSAGetLastError());
-            closesocket(ClientSocket);
-            WSACleanup();
-            return 1;
-        }
-
-        // cleanup
-        closesocket(ClientSocket);
-        WSACleanup();
-    }
-
-    return 0;
+            return 0;
 }
